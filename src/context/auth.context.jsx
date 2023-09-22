8aa3339f5548564c5d0430a3296a4fa33260ee1d
Achref LAABIDI <abidi.achref95@gmail.com>
2023-09-22 14:08:38 +0100

    setup context, services and router

---
+++ src/context/auth.context.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect, createContext } from "react";
+import authMethods from "../services/auth.service";
+
+const AuthContext = createContext();
+
+function AuthProviderWrapper(props) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
+  const storeToken = (token) => {
+    localStorage.setItem('authToken', token);
+  }
+
+  const authenticateUser = async () => {
+    try {
+      const storedToken = localStorage.getItem('authToken');
+
+      if (storedToken) {
+        const userPayload = await authMethods.verifyToken(storedToken);
+
+        setIsLoggedIn(true);
+        setIsLoading(false);
+        setUser(userPayload);
+      } else {
+        setIsLoggedIn(false);
+        setIsLoading(false);
+        setUser(null);
+      }
+    } catch (error) {
+      console.error(error);
+      setIsLoggedIn(false);
+      setIsLoading(false);
+      setUser(null);
+    }
+  }
+
+  const removeToken = () => {
+    localStorage.removeItem("authToken");
+  }
+
+  const logOutUser = () => {
+    removeToken();
+    authenticateUser();
+  }
+
+  useEffect(() => {
+    authenticateUser();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, isLoading, user, setUser, storeToken, authenticateUser, logOutUser }}>
+      {props.children}
+    </AuthContext.Provider>
+  )
+}
+
+export { AuthProviderWrapper, AuthContext };
--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router } from "react-router-dom";
+import { AuthProviderWrapper } from "./context/auth.context";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProviderWrapper>
+      <Router>
+        <App />
+      </Router>
+    </AuthProviderWrapper>
   </React.StrictMode>
 );
 
